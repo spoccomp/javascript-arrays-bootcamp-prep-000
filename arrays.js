f358b569def32adf17d1d4d04178c1c3fc097784
Richard Trapanese <richardtrapanese@gmail.com>
2018-03-13 00:18:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,15 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+function addElementToBeginningOfArray(array, element){
+  [element, ...array];
+  return array;
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(elment);
+  return array;
+}
+function addElementToEndOfArray(array, element){
+  
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  
+}
