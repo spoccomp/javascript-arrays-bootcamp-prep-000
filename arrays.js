ca62f6f57b7847d9b96f6e1ae8def89785517aa8
Richard Trapanese <richardtrapanese@gmail.com>
2018-03-13 00:07:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,1 @@
-//var chocolateBars = new Array();
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
-function addElementToBeginningOfArray(addArray, addElement){
-  var addCandy = [addElement,...addArray]
-  //addArray.unshift(addElement)
-  return addCandy
-}
-function destructivelyAddElementToBeginningOfArray(destructArray, destructElement){
-    //var destArray = new Array(chocolateBars);
-    destructArray.unshift(destructElement)
-  	return destructArray
-}
