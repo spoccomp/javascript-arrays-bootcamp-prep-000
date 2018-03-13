85a4990dc13b747f750a093f5709ede8c3ffb328
Richard Trapanese <richardtrapanese@gmail.com>
2018-03-13 00:23:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,14 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element){
-  [element, ...array];
-  return array;
+  //[element, ...array];
+  //return array;
+  array.unshift(element)
+  return array
+// but if we have a const
+const cats = ["Milo", "Garfield"]
+
+// we need a new variable:
+const moreCats = ["Felix", ...cats]
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(elment);
