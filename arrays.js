9369d27bd1eae0cae84b4451c50adaad8031bea4
Richard Trapanese <richardtrapanese@gmail.com>
2018-03-13 00:27:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element){
 
   const newAddArray = [element, ...array];
-  return ewAddArray;
+  return newAddArray;
 
 // but if we have a const
 const cats = ["Milo", "Garfield"]
